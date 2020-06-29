77ece7f480a21d52a55eaf43e6f1e3f6efbdad51
on_off <ckdgus3735@gmail.com>
2020-06-29 16:20:31 +0900

    Ball 잡고 던지기
    Ball 잡고 던지기

---
+++ Assets/02. Scripts/GrabManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabManager : MonoBehaviour
+{
+    private FixedJoint joint;
+    private GameObject grabObject;
+    private bool isTouched = false;
+
+    void Start()
+    {
+        joint = this.gameObject.GetComponent<FixedJoint>();
+    }
+
+
+    private void Update()
+    {
+        //물건을 잡을 때
+        if (isTouched && OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            joint.connectedBody = grabObject.GetComponent<Rigidbody>();
+        }
+
+        //잡은 물건을 던질 때
+        if (isTouched && OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
+        {
+            //오른쪽 Controller의 속도 산출
+            Vector3 _velocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+            grabObject.GetComponent<Rigidbody>().velocity = _velocity;
+            joint.connectedBody = null;
+            isTouched = false;
+        }
+
+        //햅틱 효과
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            OVRInput.SetControllerVibration(0.5f, 0.5f, OVRInput.Controller.RTouch);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("BALL"))
+        {
+            isTouched = true;
+            grabObject = other.gameObject;
+        }
+    }
+}
